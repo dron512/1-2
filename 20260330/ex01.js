9724f5e2faee78be761c88c27327ff8761261450
Parkmyounghoi <57881372+dron512@users.noreply.github.com>
2026-03-31 13:35:46 +0900

    0330
    0330

---
+++ 20260330/ex01.js
@@ -0,0 +1,17 @@
+console.log("안녕하세요");
+console.log("안녕하세요");
+console.log("안녕하세요");
+console.log("안녕하세요");
+console.log("안녕하세요");
+
+// 5번???
+let num = 0;
+while (num < 5) {
+    console.log("하이하이");
+    num = num + 2;
+}
+// num++ == num=num+1
+for (let num1 = 0; num1 < 5; num1 = num1 + 2) {
+    console.log(`${num1} = num1`);
+}
+// option shift 화살표 밑으로 = 밑으로 복사
