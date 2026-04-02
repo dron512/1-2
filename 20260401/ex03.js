0e0b83de700c574fffedf7b913a560c67d491e79
Parkmyounghoi <57881372+dron512@users.noreply.github.com>
2026-04-02 16:27:03 +0900

    260402
    260402

---
+++ 20260401/ex03.js
@@ -0,0 +1,12 @@
+function aa() {
+    let num = 0;
+    while (1) {
+        console.log('무한루프');
+        if (num == 2) break;
+        num += 1;
+    }
+    console.log(`num = ${num}`);
+}
+
+aa();
+// aa();
